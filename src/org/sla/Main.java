3f50a643fb33d4121d2b80964df659d554e0b6ab
jackmotter <jackmotter3@gmail.com>
2019-11-14 11:46:12 -0500

    Initial commit

---
+++ src/org/sla/Main.java
@@ -0,0 +1,30 @@
+package org.sla;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myCalc0 = "dz/dx =  ";
+        String myCalc1 = "= 3(x)^2-9";
+
+        int m = 3;
+        String m2 = "x";
+        int e = 2;
+        int m3 = -9;
+        int e2 = 0;
+
+        int p = m * e;
+        int t = m3 * e2;
+        String answer = p + m2;
+
+        System.out.println("function z " + myCalc1 + " and " + myCalc0 + answer + "+" + t);
+
+
+
+    }
+
+
+
+
+    }
+
